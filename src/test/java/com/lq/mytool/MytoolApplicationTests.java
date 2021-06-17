f0caadbded64f53b9990c5d8552f00810066830c
linqian <lq@datacreating.com>
2021-06-17 14:41:47 +0800

    项目创建

---
+++ src/test/java/com/lq/mytool/MytoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lq.mytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
