8f6605e43738366855aee061790c33d7c216764b
linqian <lq@datacreating.com>
2021-06-21 10:32:24 +0800

    slfj的加入

--- src/main/java/com/lq/mytool/controller/TestController.java
+++ src/main/java/com/lq/mytool/controller/TestController.java
@@ -1,17 +1,20 @@
 package com.lq.mytool.controller;
 
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
 
     @ApiOperation("测试接口")
     @RequestMapping(value = "/ok" ,method = RequestMethod.GET)
     public String test(){
+        log.info("Ceshi占位符：{}","LQ");
         return "success";
     }
 }
