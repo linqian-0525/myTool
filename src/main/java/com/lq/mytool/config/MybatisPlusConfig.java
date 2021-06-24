7e19bc79ff18eab2b0827dce5cbda3fac1a79413
linqian <lq@datacreating.com>
2021-06-24 14:04:51 +0800

    mybatis-plus的整合

---
+++ src/main/java/com/lq/mytool/config/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+package com.lq.mytool.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+
+    /**
+
+     * 乐观锁插件
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+}
